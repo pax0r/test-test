a5915c2d1ae2d588e248f436da9b6314a82a8cc8
Bartlomiej Biernacki <bartlomiej.biernacki@ntti3.com>
2014-07-07 13:15:21 +0200

    tests

---
+++ src/test/java/test/UberTestSuite1.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * Created by Bartłomiej Biernacki on 07.07.14.
+ */
+public class UberTestSuite1 {
+    @Test
+    public void testPassed() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testFailed() {
+        assertTrue(false);
+    }
+}
